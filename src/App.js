3b05d149abc932658ec4f260080233ea38d4e8f6
Ericka <erickamoreno529@gmail.com>
2023-06-16 14:36:23 -0700

    set state for updating task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -16,13 +17,25 @@ const TASKS = [
 ];
 
 const App = () => {
+
+  const [tasks, setTasks] = useState(TASKS);
+
+  const updateComplete = (taskToComplete) => {
+    const updateTasks = tasks.map(task => {
+      if (task.id === taskToComplete.id) {
+        return taskToComplete;
+      } return task;
+    });
+    setTasks(updateTasks); 
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div> <TaskList tasks={TASKS} updateComplete={updateComplete}/></div>
       </main>
     </div>
   );
